1e63b5de5a32ce96dd7c7722ee48bb4e14106fd3
Jameson Quave <jquave@gmail.com>
2014-07-08 02:58:36 -0500

    Some decent code highlighting

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 console.log("Run js");
 
 function escapeRegExp(string) {
+
     return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+
 }
 
 function replaceAll(string, find, replace) {
+
   return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+
 }
 
 function highlightCode() {
@@ -14,7 +18,7 @@ function highlightCode() {
   var colors = ['#8888ff', red, red, '#ccff66', '#ccff66'];
   var keywords = ['func', '(', ')', '{', '}'];
 
-  $('pre').css('background-color', '#333');
+ // $('pre').css('background-color', '#333');
   
   var types = ['CGRect', 'CGContextRef'];
 
@@ -26,36 +30,66 @@ function highlightCode() {
     colors.push(keywordColor);
   }
   
-  for(var i=0;i<keywords.length;i++) {
-    var keyword = keywords[i];
-    var color = colors[i];
 
-    console.log(keyword+' : '+color);
-    
-    var replaceText = replaceAll( $('pre').html(), keyword, '<span style="color:'+color+';">'+keyword+'</span>');
-    $('pre').html( replaceText );
+  $('pre').each(function() {
+  
+    for(var i=0;i<keywords.length;i++) {
+      var keyword = keywords[i];
+      var color = colors[i];
 
-  }  
+      console.log(keyword+' : '+color);
+      
+      var replaceText = replaceAll( $('pre').html(), keyword, '<span style="color:'+color+';">'+keyword+'</span>');
+      $(this).html( replaceText );
+
+    }  
 
 
-  // Now do background color for indents
-  color = '#000';
-  var bgcolor = "#444";
-  keyword = "  ";
-  var asciiSpace = "  ";
-  var replaceText = replaceAll( $('pre').html(), keyword, '<span style="color:'+color+';background-color:'+bgcolor+';">'+asciiSpace+'</span>');
-  $('pre').html( replaceText );
+    // Now do background color for indents
+    color = '#000';
+    var bgcolor = "#444";
+    keyword = "  ";
+    var asciiSpace = "  ";
+    var replaceText = replaceAll( $('pre').html(), keyword, '<span class="indentation">'+asciiSpace+'</span>');
+
+    $(this).html( replaceText );
+
+  });
 
 }
 
 function addLineNumber() {
-  var code = $('pre').html();
+
+  $('pre').each(function() {
+    var i = 0;
+    var code = $(this).html();
+    var lines = code.split('\n');
+    var numberedResult = "";
+    $.each(lines, function(line) {
+      var myLine = "<span class='line'><span class='lineNumber'>"+i+"</span>" + lines[line] + "</span>";
+      console.log(myLine);
+      numberedResult += myLine + "\n";
+      i++;
+    });
+    $(this).html(numberedResult);
+    i++;
+  });
+
+}
+
+function addListeners() {
+  $(".line").click(function() {
+    $("#previewTextField").val( $(this).text() );
+  });
 }
 
 // Entry point
 $(document).ready(function() {
+
   highlightCode();
   addLineNumber();
+  addListeners();
+
 });
 
 console.log("Ran js");
